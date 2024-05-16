1555e681d5339de033ea7adf351263dcad5d7c92
Dilnaz27 <dbaudunova@gmail.com>
2024-05-16 09:56:00 +0600

    feat: add constants of the project

---
+++ lib/config/constants/assets.dart
@@ -0,0 +1,20 @@
+class Assets {
+  Assets._();
+
+  static const String addCircle = 'assets/add_circle.svg';
+  static const String bookmark = 'assets/bookmark.svg';
+  static const String camera = 'assets/camera.svg';
+  static const String clock = 'assets/clock.svg';
+  static const String close = 'assets/close.svg';
+  static const String cooksCorner = 'assets/cooks_corner.svg';
+  static const String gmail = 'assets/gmail.svg';
+  static const String heart = 'assets/heart.svg';
+  static const String homeSelected = 'assets/home_selected.svg';
+  static const String homeUnselected = 'assets/home_unselected.svg';
+  static const String logout = 'assets/logout.svg';
+  static const String person = 'assets/person.svg';
+  static const String profileSelected = 'assets/profile_selected.svg';
+  static const String profileUnselected = 'assets/profile_unselected.svg';
+  static const String searchSelected = 'assets/search_selected.svg';
+  static const String searchUnselected = 'assets/search_unselected.svg';
+}
---
+++ lib/config/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+  static Color primary = const Color(0xFFFA9E31);
+  static Color inputFieldBackground = const Color(0xFFEDEDED);
+  static Color text = const Color(0xFF343434);
+  static Color secondary = const Color(0xFFFFF6DE);
+  static Color textFaded = const Color(0xFF666666);
+  static Color iconFaded = const Color(0xFF8A8A8A);
+  static Color red = const Color(0xFFFA3131);
+}
